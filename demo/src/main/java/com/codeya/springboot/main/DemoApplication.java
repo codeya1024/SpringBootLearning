99e675438be4889ed1757dfd8566daa5b87fd1d0
codeya <lili_qu@163.com>
2018-08-28 09:37:02 +0800

    spring boot demo

--- demo/src/main/java/com/codeya/springboot/main/DemoApplication.java
+++ demo/src/main/java/com/codeya/springboot/main/DemoApplication.java
@@ -4,11 +4,13 @@ import com.codeya.springboot.testyml.TokenFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages="com.codeya.springboot")
 @MapperScan(basePackages="com.codeya.springboot.**.dao")
 @EnableSwagger2
+@EnableDiscoveryClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
